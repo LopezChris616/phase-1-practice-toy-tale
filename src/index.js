6ca848c3cc16f9f0623be8ef407da0648eefe0ae
Christian Lopez <lopezchristian1610@gmail.com>
2023-04-16 00:05:56 -0400

    toys fetched from json server and displayed on page

--- src/index.js
+++ src/index.js
@@ -12,4 +12,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  getToyData();
 });
+
+function getToyData() {
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toys => {
+      displayToys(toys);
+    })
+    .catch(err => console.error(err));
+}
+
+function displayToys(toys) {
+  const toysArr = [...toys];
+
+  toysArr.forEach(toy => {
+    const toyCard = document.createElement("div");
+    const toyName = document.createElement("h2");
+    const toyPicture = document.createElement("img");
+    const toyLikes = document.createElement("p");
+    const toyLikesBtn = document.createElement("button");
+    const toyCollection = document.getElementById("toy-collection");
+
+    toyCard.classList.add("card");
+    toyPicture.classList.add("toy-avatar");
+    toyName.textContent = toy.name;
+    toyPicture.setAttribute("src", toy.image);
+    toyLikes.textContent = `${toy.likes} likes`;
+    toyLikesBtn.textContent = "Like ❤️";
+  
+    toyCard.append(toyName, toyPicture, toyLikes, toyLikesBtn);
+    toyCollection.appendChild(toyCard);
+  });
+}
